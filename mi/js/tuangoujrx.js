6843042a17e00588f92c09eb57ef085b2ffbd8c6
jack <2489770898@qq.com>
2019-01-16 13:48:28 +0800

    tuangoujrx

--- mi/js/tuangoujrx.js
+++ mi/js/tuangoujrx.js
@@ -23,3 +23,33 @@ function handleCart(){
 		oEmptyCart.style.display = 'none';
 	}
 }
+
+function animate2(obj,attr,target){
+	clearInterval(obj.timer);
+	var ispeed = 0;
+	obj.timer = setInterval(function(){
+		var current = parseFloat(getComputedStyle(obj,false)[attr]);
+		if(attr == 'opacity'){
+			current = Math.round(current * 100);
+		}
+		if(current > target){
+			iSpeed = -5;
+		}else{
+			iSpeed = 5;
+		}
+		if(Math.abs(target - current) < Math.abs(iSpeed)){
+			if(attr == 'opacity'){
+			obj.style.opacity = target / 100;
+			}else{
+				obj.style[attr] = target + 'px';
+			}
+			clearInterval(obj.timer);
+		}else{
+			if(attr == 'opacity'){
+				obj.style.opacity = (current + iSpeed)/100;
+			}else{
+				obj.style[attr] = current + iSpeed + 'px';
+			}
+		}
+	},30)
+}
